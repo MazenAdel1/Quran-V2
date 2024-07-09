2e0d806d1522badafa9dffa428257f6f50bfad81
Mazen Adel <ma134652134652@gmail.com>
2024-07-09 21:27:06 +0300

    fix: fix bookmarks empty UI

--- components/list/List.tsx
+++ components/list/List.tsx
@@ -84,7 +84,7 @@ export default function List({
           ))
         ) : (
           <h2
-            className={`absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 ${sheet ? "text-2xl" : "text-3xl"} text-white`}
+            className={`absolute left-1/2 top-1/2 w-full -translate-x-1/2 -translate-y-1/2 text-center ${sheet ? "text-2xl" : "text-2xl sm:text-3xl"} text-white`}
           >
             ليس لديك مرجعيات
           </h2>
