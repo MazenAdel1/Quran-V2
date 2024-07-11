e3ad608c4186e244b9b49eb4aecb0a0c2d6b1c1d
Mazen Adel <ma134652134652@gmail.com>
2024-07-11 22:16:55 +0300

    refactor: separate Ayahs to an independent component

---
+++ components/readingPage/Ayahs.tsx
@@ -0,0 +1,61 @@
+"use client";
+
+import React, { Fragment, useRef } from "react";
+import SurahTitle from "./SurahTitle";
+
+type AyahProps = {
+  text: string;
+  numberInSurah: number;
+  surah: {
+    number: number;
+    name: string;
+    revelationType: "Meccan" | "Medinan";
+    numberOfAyahs: number;
+  };
+};
+
+export default function Ayahs({ ayahs, page }: any) {
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  return (
+    <div
+      ref={containerRef}
+      className="xs:text-[22px] xs:leading-[2.6] h-full w-full overflow-hidden font-amiri text-[20px] leading-[2.4] text-white sm:text-[24px] sm:leading-[2.7] md:text-[26px] md:leading-[2.6] lg:text-[29px] lg:leading-[2.9] xl:text-[32px] xl:leading-[3]"
+    >
+      {ayahs.map((ayah: AyahProps) =>
+        ayah.numberInSurah === 1 ? (
+          <Fragment key={ayah.numberInSurah}>
+            <SurahTitle surahName={ayah.surah.name} />
+            {ayah.text.includes("ٱللَّهِ ٱلرَّحۡمَـٰنِ ٱلرَّحِیمِ") &&
+            page !== 1 ? (
+              <>
+                <span
+                  key={ayah.numberInSurah}
+                  className="block text-center font-camel font-bold"
+                >
+                  بسم الله الرحمـٰن الرحيم
+                </span>
+                <p className="inline px-1 [word-spacing:_-3px] sm:[word-spacing:_-4px]">{`${ayah.text.split("ٱللَّهِ ٱلرَّحۡمَـٰنِ ٱلرَّحِیمِ")[1]}`}</p>
+                {` \u06DD${ayah.numberInSurah} `}
+              </>
+            ) : (
+              <>
+                <p className="inline px-1 [word-spacing:_-3px] sm:[word-spacing:_-4px]">
+                  {ayah.text}
+                </p>
+                {` \u06DD${ayah.numberInSurah} `}
+              </>
+            )}
+          </Fragment>
+        ) : (
+          <Fragment key={ayah.numberInSurah}>
+            <p className="inline px-1 [word-spacing:_-3px] sm:[word-spacing:_-4px]">
+              {ayah.text}
+            </p>
+            {` \u06DD${ayah.numberInSurah} `}
+          </Fragment>
+        ),
+      )}
+    </div>
+  );
+}
