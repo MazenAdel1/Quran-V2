ea22b9d32a8f0860b8688fdc0cdb7d3dd950e435
ma134652134652 <ma134652134652@gmail.com>
2024-07-16 09:16:42 +0000

    fix: reverse bookmarks so that new will be first

--- components/list/List.tsx
+++ components/list/List.tsx
@@ -81,7 +81,7 @@ export default function List({
               page={bookmark.page}
               onClick={() => sheetCloseRef.current?.click()}
             />
-          ))
+          )).reverse()
         ) : (
           <h2
             className={`absolute left-1/2 top-1/2 w-full -translate-x-1/2 -translate-y-1/2 text-center ${sheet ? "text-2xl" : "text-2xl sm:text-3xl"} text-white`}
