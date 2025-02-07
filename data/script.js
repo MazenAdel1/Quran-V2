9be66a22e623054602ebe21e33d003bfe3a16e87
Mazen Adel <ma134652134652@gmail.com>
2025-02-07 17:42:33 +0200

    feat(script.js): add script for fetching verses

--- data/script.js
+++ data/script.js
@@ -1,6 +1,6 @@
 const fs = require("node:fs");
 
-async function getData() {
+async function getChapters() {
   const R = await fetch(`https://api.quran.com/api/v4/chapters`);
   const D = await R.json();
 
@@ -20,4 +20,48 @@ async function getData() {
   });
 }
 
-getData();
+getChapters();
+
+async function getAyahs() {
+  const DATA = [];
+  for (let i = 1; i <= 604; i++) {
+    try {
+      const res = await fetch(`https://api.alquran.cloud/v1/page/${i}`);
+      if (!res.ok) {
+        throw new Error(`Failed to fetch data for page ${i}`);
+      }
+      const D = await res.json();
+
+      const surahs = Object.values(D.data.surahs).map((surah) => ({
+        name: surah.name,
+        number: surah.number,
+        numberOfAyahs: surah.numberOfAyahs,
+        revelationType: surah.revelationType,
+      }));
+
+      const ayahs = D.data.ayahs.map((ayah) => ({
+        numberInSurah: ayah.numberInSurah,
+        surah: {
+          name: ayah.surah.name,
+        },
+        text: ayah.text,
+      }));
+
+      DATA.push({ surahs, ayahs });
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  fs.appendFile(
+    "./data/ayahs.ts",
+    `export const ayahs = ${JSON.stringify(DATA)}`,
+    (err) => {
+      if (err) {
+        console.error(err);
+      }
+    },
+  );
+}
+
+getAyahs();
