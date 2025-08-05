cb61ec79c1ce66abe0e8267ca40e9ac3fec1220a
MazenAdel1 <mazenadel.dev@gmail.com>
2025-08-06 00:30:45 +0300

    revert: disable unavailable feature

--- components/list/SectionNavigation.tsx
+++ components/list/SectionNavigation.tsx
@@ -13,11 +13,10 @@ import {
 export default function SectionNavigation() {
   const SECTIONS = [
     { title: "قراءة", available: true },
-    { title: "استماع", available: true },
+    { title: "استماع", available: false },
     { title: "تعلم", available: false },
   ];
 
-  // TODO make the listen section
   return (
     <>
       <nav className="flex justify-end lg:absolute lg:-right-[20%] lg:h-full">
@@ -33,7 +32,7 @@ export default function SectionNavigation() {
                     rounded: "md",
                     padding: "md",
                     size: "full",
-                    className: `relative block text-center text-2xl font-bold ${!section.available ? "pointer-events-none text-white before:absolute before:left-0 before:top-0 before:flex before:h-full before:w-full before:items-center before:justify-center before:bg-black/40 before:text-base before:content-['تحت_التطوير...']" : ""}`,
+                    className: `relative block text-center text-2xl font-bold ${!section.available ? "pointer-events-none text-white before:absolute before:top-0 before:left-0 before:flex before:h-full before:w-full before:items-center before:justify-center before:bg-black/40 before:text-base before:content-['تحت_التطوير...']" : ""}`,
                   }),
                 )}
               >
@@ -43,15 +42,15 @@ export default function SectionNavigation() {
           ))}
         </ul>
         <Select>
-          <SelectTrigger className="w-36 bg-light-orange text-black dark:bg-light-navy dark:text-white lg:hidden">
+          <SelectTrigger className="bg-light-orange dark:dark:bg-light-navy w-36 text-black lg:hidden dark:text-white">
             <SelectValue placeholder="الأقسام" />
           </SelectTrigger>
-          <SelectContent className="flex flex-col items-center justify-center bg-light-orange dark:bg-light-navy">
+          <SelectContent className="bg-light-orange dark:bg-light-navy flex flex-col items-center justify-center">
             {SECTIONS.map((section) => (
               <Link
                 href={"/"}
                 key={section.title}
-                className={`relative block size-full flex-1 bg-light-orange py-1 text-center text-xl text-black transition hover:bg-[#ffb777] dark:bg-light-navy dark:text-white dark:hover:bg-navy  ${!section.available ? "pointer-events-none text-white before:absolute before:left-0 before:top-0 before:flex before:h-full before:w-full before:items-center before:justify-center before:bg-black/40 before:text-base before:content-['...تحت_التطوير']" : ""}`}
+                className={`bg-light-orange dark:bg-light-navy dark:hover:bg-navy relative block size-full flex-1 py-1 text-center text-xl text-black transition hover:bg-[#ffb777] dark:text-white ${!section.available ? "pointer-events-none text-white before:absolute before:top-0 before:left-0 before:flex before:h-full before:w-full before:items-center before:justify-center before:bg-black/40 before:text-base before:content-['...تحت_التطوير']" : ""}`}
               >
                 {section.title}
               </Link>
